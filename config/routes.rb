abcf42827d7674466939cf8d7761bfbd9b735733
yumeka2024 <154503350+yumeka2024@users.noreply.github.com>
2024-02-14 06:21:22 +0000

    [Update]sign_in_out遷移先

--- config/routes.rb
+++ config/routes.rb
@@ -1,5 +1,10 @@
 Rails.application.routes.draw do
+
+  get 'homes/about' => 'homes#about', as: 'about'
+
   devise_for :users
   root to: "homes#top"
+
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
+
 end
