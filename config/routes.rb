501491e6e561a9535c4f9cfe8941b8c23635396f
yumeka2024 <154503350+yumeka2024@users.noreply.github.com>
2024-02-15 09:34:48 +0000

    [Add]コメント機能

--- config/routes.rb
+++ config/routes.rb
@@ -1,8 +1,11 @@
 Rails.application.routes.draw do
 
   resources :users, only: [:show, :edit, :update]
-  
-  resources :post_images, only: [:new, :create, :index, :show, :destroy]
+
+  resources :post_images, only: [:new, :create, :index, :show, :destroy] do
+    resources :post_comments, only: [:create, :destroy]
+  end
+
 
   get 'homes/about' => 'homes#about', as: 'about'
 
